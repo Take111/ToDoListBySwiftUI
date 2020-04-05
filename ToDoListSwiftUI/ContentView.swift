7eb9c12b23705445e2f1a74a06eaa7d2aae7fdab
Take111 <maxmaxastea@gmail.com>
2020-04-05 14:58:42 +0900

    Initial Commit

---
+++ ToDoListSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoListSwiftUI
+//
+//  Created by 竹ノ内愛斗 on 2020/04/05.
+//  Copyright © 2020 竹ノ内愛斗. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
