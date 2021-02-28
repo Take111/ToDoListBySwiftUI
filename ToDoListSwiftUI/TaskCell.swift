f8b644754da6b6167068b9dc3fecb56f32e0cb15
Take111 <maxmaxastea@gmail.com>
2021-02-28 13:30:39 +0900

    feat: create View

---
+++ ToDoListSwiftUI/TaskCell.swift
@@ -0,0 +1,21 @@
+//
+//  TaskCell.swift
+//  ToDoListSwiftUI
+//
+//  Created by 竹ノ内愛斗 on 2021/02/28.
+//  Copyright © 2021 竹ノ内愛斗. All rights reserved.
+//
+
+import SwiftUI
+
+struct TaskCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TaskCell_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskCell()
+    }
+}
---
+++ ToDoListSwiftUI/TodoListRow.swift
@@ -0,0 +1,21 @@
+//
+//  TodoListRow.swift
+//  ToDoListSwiftUI
+//
+//  Created by 竹ノ内愛斗 on 2020/04/05.
+//  Copyright © 2020 竹ノ内愛斗. All rights reserved.
+//
+
+import SwiftUI
+
+struct TodoListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TodoListRow_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoListRow()
+    }
+}
